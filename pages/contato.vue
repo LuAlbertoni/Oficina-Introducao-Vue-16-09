299c9a15e6ed9d5944abfae7de94c4f8c6c1033d
LuAlbertoni <lualbertoni@protonmail.com>
2022-09-16 10:18:55 -0300

    Criação página contato

---
+++ pages/contato.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Contato</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "contato",
+    head() {
+        return {
+            title: "Contato"
+        }
+    },
+}
+</script>
