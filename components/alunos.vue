bf437d00a54696c541d016e2fa249190f2bcb77a
LuAlbertoni <lualbertoni@protonmail.com>
2022-09-16 10:18:42 -0300

    Adição lista de alunos

--- components/alunos.vue
+++ components/alunos.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
         <h3>Lista de alunos (Componente):</h3>
-
+        <p v-for="aluno in lista">{{aluno}}</p>
 
     </div>
 </template>
 
 <script>
 export default {
-    name: "alunos"
+    name: "alunos",
+    props: {
+        lista: {
+            type: Array,
+            required: true
+        }
+    }
 }
 </script>
