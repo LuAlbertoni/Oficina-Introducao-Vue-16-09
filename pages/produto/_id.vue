92a82eca53e6aa507986c018158f2351526453e0
LuAlbertoni <lualbertoni@protonmail.com>
2022-09-16 10:24:40 -0300

    Adição de slug dinâmico

---
+++ pages/produto/_id.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Produto - {{this.$route.params.id}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "produto",
+    mounted() {
+        
+    }
+}
+</script>
